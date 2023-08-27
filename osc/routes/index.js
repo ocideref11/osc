9ce060b498470da10e6e776d5f215b6cef6929d7
federicodonati07 <realeaquila.929@gmail.com>
2023-08-27 19:13:46 +0200

    osc, Alpha[0.5]

--- osc/routes/index.js
+++ osc/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'OSC' });
 });
 
+router.get("/driver", function(req, res, next){
+  res.render("dlogin", {title:"OSC"})
+})
+
+/* POST home page.*/
+router.post("/driver", function(req, res, next){
+  res.render("dindex", {title:"OSC"})
+})
+
 module.exports = router;
